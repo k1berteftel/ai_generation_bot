9cada4d40bd445597f5ed4d1db062691f9402bad
k1berteftel <kkulis985@gmail.com>
2025-07-28 02:49:34 +0300

    fix

--- keyboards/inline.py
+++ keyboards/inline.py
@@ -166,6 +166,7 @@ def get_prompt_keyboard(user_id: int, selected_model: str) -> InlineKeyboardMark
         )
 
     if selected_model == 'Sora - Генерация изображений':
+        back = 'photo_menu'
         return InlineKeyboardMarkup(
             inline_keyboard=[
                 [InlineKeyboardButton(text="ℹ️ Инструкция", url="https://t.me/veo3guide")],
